41331e4801aa15fd6700a41468b0f14cc117612f
josh <lowejosh50@gmail.com>
2019-02-11 07:23:19 +1100

    added scaling and interval rendering for testing

--- js/vectorbrush.js
+++ js/vectorbrush.js
@@ -1,5 +1,6 @@
 // === GLOBAL VARS ===
 let vCanv = document.getElementById('vector-canvas');
+let scale = 1;
 
 
 
@@ -7,14 +8,24 @@ let vCanv = document.getElementById('vector-canvas');
 
 // === CANVAS RENDERING ===
 function VectorCanvas(props) {
-        return (
-            // Working area
-            <svg className="vector-canvas" width="100%" height="100%">
-                {/* Actual canvas */}
-                <rect x={vCanv.offsetWidth/2 - props.width/2} y={vCanv.offsetHeight/2 - props.height/2} width={props.width} height={props.height} fill={props.colour} />
-            </svg>
-        );
+    let w = props.width * scale;
+    let h = props.height * scale;
+    let midX = vCanv.offsetWidth/2 - w/2;
+    let midY = vCanv.offsetHeight/2 - h/2;
+
+    return (
+        // Working area
+        <svg width="100%" height="100%">
+            {/* Actual canvas */}
+            <rect x={midX} y={midY} width={w} height={h} fill={props.colour} />
+        </svg>
+    );
 }
 
+
+// TESTING
+setInterval(renderAll, 100);
 const element = <VectorCanvas width="500" height="500" colour="#FCFCFC" />;
-ReactDOM.render(element, vCanv);
+function renderAll() {
+    ReactDOM.render(element, vCanv);
+}
