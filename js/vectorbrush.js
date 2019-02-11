497353302045af31abe06ee25d0f9a4b52c63b85
josh <lowejosh50@gmail.com>
2019-02-11 13:54:05 +1100

    added automatic scaling

--- js/vectorbrush.js
+++ js/vectorbrush.js
@@ -1,17 +1,21 @@
 // === GLOBAL VARS ===
+// Default canvas vars
 let vCanv = document.getElementById('vector-canvas');
 let scale = 1;
-
-
-
-
+let defWidth = 500;
+let defHeight = 500;
+let element = <VectorCanvas width={defWidth} height={defHeight} colour="#FCFCFC" />;
+let cWidth = defWidth * scale;
+let cHeight = defHeight * scale;
 
 // === CANVAS RENDERING ===
 function VectorCanvas(props) {
-    let w = props.width * scale;
-    let h = props.height * scale;
-    let midX = vCanv.offsetWidth/2 - w/2;
-    let midY = vCanv.offsetHeight/2 - h/2;
+    let tW = props.totalWidth;
+    let tH = props.totalHeight;
+    let w = props.width;
+    let h = props.height;
+    let midX = tW/2 - w/2;
+    let midY = tH/2 - h/2;
 
     return (
         // Working area
@@ -22,10 +26,44 @@ function VectorCanvas(props) {
     );
 }
 
-
 // TESTING
-setInterval(renderAll, 100);
-const element = <VectorCanvas width="500" height="500" colour="#FCFCFC" />;
+setInterval(renderAll, 50);
+
 function renderAll() {
+    // Vars
+    let totalWidth = vCanv.offsetWidth;
+    let totalHeight = vCanv.offsetHeight;
+
+    if (!totalWidth) {
+        cWidth = 500;
+        totalWidth = 549;
+    }
+
+    // === AUTOMATIC SCALING ===
+    if ((totalWidth - cWidth) < 50) {
+        scale = (totalWidth - 75) / defWidth;
+        cHeight = defHeight * scale;
+    } else if (((totalWidth - cWidth) > 100) && scale < 1) {
+        scale = (totalWidth - 75) / defWidth;
+        cHeight = defHeight * scale;
+    }
+    if ((totalHeight - cHeight) < 50) {
+        let tempScale = (totalHeight - 75) / defHeight;
+        if (tempScale < scale) {
+            scale = tempScale;
+        }
+    } else if (((totalHeight - cHeight) > 100) && scale < 1) {
+        let tempScale = (totalHeight - 75) / defHeight;
+        if (tempScale < scale) {
+            scale = tempScale;
+        }
+    }
+
+    // Update vars
+    cWidth = defWidth * scale;
+    cHeight = defHeight * scale;
+    console.log(scale);
+
+    element = <VectorCanvas width={cWidth} height={cHeight} totalWidth={totalWidth} totalHeight={totalHeight} colour="#FCFCFC" />;
     ReactDOM.render(element, vCanv);
 }
